87ecc3ccb6004258a005f7dc5c14223c2059f5c1
paviethra <62457057+paviethra@users.noreply.github.com>
2022-10-30 14:28:09 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const bodyParser = require('body-parser');
+const moment = require('moment');
+app.locals.moment = moment;
+
+
+app.use(express.static('public'));
+app.set('view engine','ejs');
+
+app.use(express.urlencoded({extended:true}));
+app.use('/',require('./routes/news'));
+
+app.set('views','./views');
+
+app.listen(port,()=>{
+    console.log(`Listening to the port ${port}`);
+});
+
